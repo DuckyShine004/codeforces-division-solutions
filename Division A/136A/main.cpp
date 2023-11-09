25dfc063ed88eddef2aea33cc72968bc1b5d63b6
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-09 18:26:24 +1300

    feat: Added Solution To Problem 136A

---
+++ Division A/136A/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> A(n);
+
+	int p;
+
+	for (int i = 0; i < n; i++) {
+		cin >> p;
+
+		A[p - 1] = i + 1;
+	}
+
+	for (int j = 0; j < n; j++) {
+		cout << A[j] << " ";
+	}
+}
