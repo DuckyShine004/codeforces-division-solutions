57811763e22f95ae560c084f2879375cbc1b3512
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-13 19:42:08 +1300

    feat: Added Solution To Problem 630A

---
+++ Division A/630A/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	double n;
+	cin >> n;
+
+	cout << 25;
+}
