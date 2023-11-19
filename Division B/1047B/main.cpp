634c66f88ff0be3421f0fa6fd4d13f0646331e1f
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-11-20 01:38:57 +1300

    feat: Added Solution To Problem 1047B

---
+++ Division B/1047B/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	long long x, y, dx, dy;
+	long long d, k;
+
+	d = 0;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		k = x + y;
+
+		if (d < k) {
+			dx = x;
+			dy = y;
+
+			d = k;
+		}
+	}
+
+	cout << dx + dy;
+}
