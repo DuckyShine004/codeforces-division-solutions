650ff65124c5285e8cb6ccb861a22542d0a2809f
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-03 01:05:36 +1300

    feat: Added Solution To Problem 1107B

---
+++ Division B/1107B/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve(ll k, ll x) {
+	cout << (k > 1 ? x + (9 * (k - 1)) : x) << "\n";	
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    int n;
+	ll k, x;
+    cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> k >> x;
+		solve(k, x);
+	}
+}
