ce571929f4eb2d8fc0dff2a95cb76dafbfdbe6e2
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-23 20:50:43 +1300

    refactor: Added Solution To Problem 1359B

--- division-b/1359B/main.cpp
+++ division-b/1359B/main.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+const int INF = 1e9;
+
 void solve() {
-	int n;
-	cin >> n;
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
 
-	int m = 3 * n;
-	int k = m / 4;
+    vector<string> arr(n);
+    vector<vector<int>> dp(n, vector<int>(m + 1));
 
-	string s(k, '9');
-	string t(n - k, '8');
+    int res = 0;
 
-	cout << s + t << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= m; j++) {
+            dp[i][j] = INF;
+
+            if (arr[i][j - 1] == '.') {
+                dp[i][j] = min(dp[i][j], dp[i][j - 1] + x);
+
+                if (j > 1 && arr[i][j - 2] == '.') {
+                    dp[i][j] = min(dp[i][j], dp[i][j - 2] + y);
+                }
+            } else {
+                dp[i][j] = dp[i][j - 1];
+            }
+        }
+
+        res += dp[i][m];
+    }
+
+    cout << res << "\n";
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
 
-	while (t--) {
-		solve();
-	}
+    while (t--) {
+        solve();
+    }
 }
