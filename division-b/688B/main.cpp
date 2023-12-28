f674ac1fde50c11d9011b741c15822e14d6acf8c
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-28 21:05:24 +1300

    feat: Added Solution To Problem 688B

---
+++ division-b/688B/main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    cout << s;
+
+    reverse(s.begin(), s.end());
+
+    cout << s;
+}
