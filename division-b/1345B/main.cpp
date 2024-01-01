2ecc5ae7de61a463e9f0386e983f0f6aea9ccbd2
Gallon Zhou <gallonzhou04@gmail.com>
2024-01-01 14:18:33 +1300

    feat: Added Solution To Problem 1345B

--- division-b/1345B/main.cpp
+++ division-b/1345B/main.cpp
@@ -2,10 +2,64 @@
 
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+typedef long long ll;
 
-	
+ll get_height(ll n) { return ((3 * n * (n + 1)) / 2) - n; }
+
+void solve() {
+    int n;
+    cin >> n;
+
+    ll l, r, h, k;
+    bool flag;
+
+    int res = 0;
+
+    while (n > 1) {
+        flag = false;
+
+        l = 0;
+        r = n / 2;
+
+        while (l <= r) {
+            k = l + (r - l) / 2;
+            h = get_height(k);
+
+            if (h == n) {
+                flag = true;
+                n -= h;
+
+                break;
+            }
+
+            if (h < n) {
+                l = k + 1;
+            } else {
+                r = k - 1;
+            }
+        }
+
+        res++;
+
+        if (flag) {
+            continue;
+        }
+
+        n -= get_height(r);
+    }
+
+    cout << res << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
 }
