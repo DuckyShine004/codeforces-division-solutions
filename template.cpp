57dd150dded5f68bdd9d495a4428a7d212ed43a4
Gallon Zhou <gallonzhou04@gmail.com>
2024-01-03 02:04:51 +1300

    feat: Added Debugging To Template

--- template.cpp
+++ template.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
+#include <chrono>
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <map>
 #include <set>
@@ -7,6 +9,7 @@
 #include <vector>
 
 using namespace std;
+using namespace std::chrono;
 
 #define f first
 #define s second
@@ -34,6 +37,8 @@ typedef vector<pll> vpll;
 const int MAX_N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
+const pii d4[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+const pii d8[8] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, -1}};
 
 template <typename T> void print_soln(const T &x) {
     for (auto it = x.begin(); it != x.end();) {
@@ -51,6 +56,25 @@ template <typename T> T gcd(T a, T b) {
 
     return gcd(b, a % b);
 }
+
+int recur_depth = 0;
+
+// clang-format off
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+// clang-format on
+#else
+#define dbg(x)
+#endif
+template <typename Ostream, typename Cont> typename enable_if<is_same<Ostream, ostream>::value, Ostream &>::type operator<<(Ostream &os, const Cont &v) {
+    os << "[";
+    for (auto &x : v) {
+        os << x << ", ";
+    }
+
+    return os << "]";
+}
+template <typename Ostream, typename... Ts> Ostream &operator<<(Ostream &os, const pair<Ts...> &p) { return os << "{" << p.first << ", " << p.second << "}"; }
 
 void solve() {}
 
