e441be3c41f14435a7be05bd1366df5c90cd3108
Gallon Zhou <gallonzhou04@gmail.com>
2024-01-02 23:56:25 +1300

    feat: Added Solution To Problem 1099B

--- division-b/1099B/main.cpp
+++ division-b/1099B/main.cpp
@@ -1,12 +1,53 @@
+#include <cmath>
 #include <iostream>
-#include <queue>
 
 using namespace std;
+
+typedef long long ll;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    
+    ll n, k, v;
+    int l, r, m;
+    bool flag;
+
+    cin >> n;
+
+    k = ceil(sqrt(n));
+
+    ll res = 1e10;
+
+    for (ll i = 1; i <= k; i++) {
+        flag = true;
+
+        l = 0;
+        r = n;
+
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            v = i * m;
+
+            if (v == n) {
+                res = min(res, i + m);
+                flag = false;
+
+                break;
+            }
+
+            if (v < n) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        if (flag) {
+            res = min(res, i + r + 1);
+        }
+    }
+
+    cout << res;
 }
