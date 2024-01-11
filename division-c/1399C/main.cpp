b4aebd3e3e9e1c276b9c2af5fd43526c9bca5bc0
Gallon Zhou <gallonzhou04@gmail.com>
2024-01-11 16:48:53 +1300

    feat: Added Solution to Problem 1099C

--- division-c/1399C/main.cpp
+++ division-c/1399C/main.cpp
@@ -5,58 +5,58 @@
 using namespace std;
 
 void solve() {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	vector<int> A(n);
+    vector<int> A(n);
 
-	for (int &num : A) {
-		cin >> num;
-	}
+    for (int &num : A) {
+        cin >> num;
+    }
 
-	sort(A.begin(), A.end());
+    sort(A.begin(), A.end());
 
-	int _min = 2 * A[0];
-	int _max = 2 * A[n-1];
+    int _min = 2 * A[0];
+    int _max = 2 * A[n - 1];
 
-	int res = -1;
+    int res = -1;
 
-	for (int s = _min; s <= _max; s++) {
-		int l = 0;
-		int r = n-1;
+    for (int s = _min; s <= _max; s++) {
+        int l = 0;
+        int r = n - 1;
 
-		int count = 0;
+        int count = 0;
 
-		while (l < r) {
-			int sum = A[l] + A[r];
+        while (l < r) {
+            int sum = A[l] + A[r];
 
-			if (sum == s) {
-				count++;
+            if (sum == s) {
+                count++;
 
-				l++;
-				r--;
-			} else if (sum < s) {
-				l++;
-			} else{
-				r--;
-			}
-		}
+                l++;
+                r--;
+            } else if (sum < s) {
+                l++;
+            } else {
+                r--;
+            }
+        }
 
-		res = max(res, count);
-	}
+        res = max(res, count);
+    }
 
-	cout << res << endl;
+    cout << res << endl;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
 
-	while (t--) {
-		solve();
-	}
+    while (t--) {
+        solve();
+    }
 }
